51a39f072891df1e0068e0b6b22149fe4fc2e264
Josh <joshua.engel87@gmail.com>
2015-04-28 11:30:37 -0500

    Added In Final Core Pages
    Functionality Still needs to be filled out

---
+++ materialWP-child/about-page.php
@@ -0,0 +1,79 @@
+<div class="heroBodyBG">
+    <div class="outerContentCont">
+        <div class="half-hero">
+        </div>
+
+<div class="card">
+      <div style="text-align: center; color: #000; padding-top: 15px;">
+          <h1>About Creative Cypher</h1>
+      </div>
+
+      <div class="innerContainer">
+        
+        <div class="troyimage">
+        </div>
+        
+        <div class="biobox"> 
+          
+          <div class="biostate" >
+            
+            <h1>Bio</h1>
+            
+            <p>The Creative Cypher is a conscientious community of innovative   directors, producers, screenwriters, film editors, sound engineers, composers and performers, founded in 2013 by Troy Pryor. 501(c)(3)
+            </p>
+            
+          </div>
+          
+        </div>
+        
+      <div class="misbox">
+        
+        <div class="misstate"> 
+          
+          <h1>Mission</h1>
+          
+          <p>To be a resource and support system for its membership. To provide mentoring and training to those interested in a career in the entertainment industry. </p>
+          
+        </div>
+        
+      </div>
+        
+      <div class="visbox">
+      <div class="visstate">
+        <h1>Vision</h1>
+        <p>Through guidance and leadership the goal of Creative Cypher is to broaden access, expand opportunity, and increase diversity in the general Chicago creative community.
+        </p>
+      </div>
+     </div>
+     
+    <div class="visbox">
+      <div class="visstate">
+        <h1 style="padding-bottom: 20px;">The Formula</h1>
+        <h4 style="text-decoration: underline">Artist Community</h3>
+        <p style="padding-bottom: 15px;">A live and digital community, safe for members to share & pitch ideas, plus youth mentoring programs and exclusive invites to VIP events.
+        </p>
+        <h4 style="text-decoration: underline">Development</h3>
+        <p style="padding-bottom: 15px;">Counseling, coverage, table reads, staged readings, PR, and marketing plans.
+        </p>
+        <h4 style="text-decoration: underline">Production</h3>
+        <p style="padding-bottom: 15px;">Access to production teams, strategists, and trade discounts (rentals).
+        </p>
+        <h4 style="text-decoration: underline">Distribution & Exhibition</h3>
+        <p style="padding-bottom: 15px;">Access to a network of aggregators to showcase your brand and monetize content (Digital development, publicists, social media/marketing strategists, web designers, graphic artists, tech support, analytics, and event hosting).
+        </p>
+      </div>
+     </div>
+     
+     <div class="visbox">
+        <div class="visstate">
+        <h1>The Seven Gems</h1>
+            <h3>#CYPHERLIVE</h3>
+            <h3>THE INTENSIVE</h3>
+            <h3>THE FILM HACK-A-THON</h3>
+            <h3>THE GALLERY</h3>
+            <h3>THE FILM FEAST</h3>
+            <h3>THE PRIVATE SCREENING</h3>
+            </div>
+            </div>
+</div>
+</div>
