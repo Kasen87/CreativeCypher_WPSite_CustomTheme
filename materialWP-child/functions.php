95aae9ed726c50cd6603a95410cc46f4fdde8c36
Josh <joshua.engel87@gmail.com>
2015-04-28 19:49:02 -0500

    jQuery Added In, along with custom JS files
    Functionality will be fairly simple to implement now.

--- materialWP-child/functions.php
+++ materialWP-child/functions.php
@@ -1,9 +1,11 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles(){
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css');
+    wp_register_script( 'custom-js', get_stylesheet_directory_uri() . '/js/custom.js' );
+    wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/js/custom.js', array('jquery'), '', true);
 }
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles');
 
 /**
 *
---
+++ materialWP-child/js/custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($) {
+    $(".customMenuBtn").click(function($){
+        jQuery("body").animate({"margin-left":"-200px"}, 500, 'swing');
+    });
+    
+});
